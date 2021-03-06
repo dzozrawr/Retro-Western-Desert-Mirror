5dfb0cebfdc5096148b726716a326f6bf194c155
DESKTOP-AV50T2T\Admin <jovan_96@live.ru>
2021-03-06 23:43:14 +0100

    first commit: added most of the assets, movement, animations and shooting

---
+++ Assets/Scripts/CharacterController.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterController : MonoBehaviour
+{
+    private const int extraJumpValue = 1;
+
+    public float charSpeed = 250;
+    public float jumpForce= 7f;
+    
+
+    private bool facingRight = true;
+    private Rigidbody2D rb;
+
+    private bool isGrounded;
+    public Transform groundCheck;
+    public float checkRadius=0.01f;
+    public LayerMask whatIsGround;
+
+    public int extraJumps= extraJumpValue;
+
+    public Animator animator;
+
+    float horizontalMove = 0f;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        horizontalMove = Input.GetAxisRaw("Horizontal") * charSpeed;
+
+        animator.SetFloat("Speed", Mathf.Abs(horizontalMove));
+
+
+        if (isGrounded == true)
+        {
+            extraJumps = extraJumpValue;
+            if(rb.velocity.y<=0) animator.SetBool("isJumping", false);
+        }
+
+        if (Input.GetButtonDown("Jump") && isGrounded)
+        {
+            rb.velocity = Vector2.up * jumpForce;
+            animator.SetBool("isJumping", true);
+        }
+        else if ((Input.GetButtonDown("Jump") && extraJumps > 0))
+        {
+            rb.velocity = Vector2.up * jumpForce;
+            extraJumps--;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, checkRadius, whatIsGround);
+
+        
+        rb.velocity = new Vector2(horizontalMove * Time.fixedDeltaTime, rb.velocity.y);
+
+        if(facingRight==false && Input.GetAxisRaw("Horizontal") > 0)
+        {
+            Flip();
+        }
+        else if(facingRight==true && Input.GetAxisRaw("Horizontal") < 0)
+        {
+            Flip();
+        }
+    }
+    // Update is called once per frame
+
+
+    void Flip()
+    {
+        facingRight = !facingRight;
+        Vector3 scaler = transform.localScale;
+        scaler.x *= -1;
+        transform.localScale = scaler;
+    }
+}
---
+++ Assets/Scripts/ShootingScript.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShootingScript : MonoBehaviour
+{
+    public float fireRate = 0f;
+    public float damage = 10f;
+    public LayerMask whatToHit;
+
+    public GameObject bulletPrefab=null;
+
+    public float bulletSpeed=250f;
+
+    float timeToFire = 0;
+
+    Transform firePoint;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        firePoint = transform.Find("FirePoint");
+        if (firePoint == null)
+        {
+            Debug.LogError("No firepoint.");
+        }
+
+        if (bulletPrefab == null)
+        {
+            Debug.LogError("No bulletPrefab.");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (fireRate == 0)
+        {
+            if (Input.GetButtonDown("Fire1"))
+            {
+                Shoot();
+            }
+        }
+        else if (Input.GetButtonDown("Fire1")&& Time.time>timeToFire)
+        {
+            timeToFire = Time.time + 1 / fireRate;
+            Shoot();
+        }
+    }
+
+    void Shoot()
+    {
+        GameObject bullet= Instantiate(bulletPrefab,firePoint.position,firePoint.rotation);
+
+        bullet.GetComponent<Rigidbody2D>().AddForce(new Vector2(bulletSpeed, 0f));
+    }
+}
